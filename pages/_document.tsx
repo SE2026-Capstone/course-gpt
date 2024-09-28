ac42b0209e439e9815638b19162cf235206ccb08
Eric Xu <ericxu2048@gmail.com>
2024-09-28 15:43:51 -0400

    Add MUI

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,9 +1,26 @@
-import { Html, Head, Main, NextScript } from "next/document";
+import {
+  Html,
+  Head,
+  Main,
+  NextScript,
+  DocumentContext,
+  DocumentProps,
+} from "next/document";
+import {
+  AppCacheProvider,
+  DocumentHeadTags,
+  DocumentHeadTagsProps,
+  documentGetInitialProps,
+} from "@mui/material-nextjs/v14-pagesRouter";
 
-export default function Document() {
+export default function Document(props: DocumentProps & DocumentHeadTagsProps) {
   return (
     <Html lang="en">
-      <Head />
+      <AppCacheProvider {...props}>
+        <Head>
+          <DocumentHeadTags {...props} />
+        </Head>
+      </AppCacheProvider>
       <body>
         <Main />
         <NextScript />
@@ -11,3 +28,8 @@ export default function Document() {
     </Html>
   );
 }
+
+Document.getInitialProps = async (ctx: DocumentContext) => {
+  const finalProps = await documentGetInitialProps(ctx);
+  return finalProps;
+};
